4bdb0eb6ed792154280030ba031d409cf2fa53e1
NtonBala <balashov.anton.1983@gmail.com>
2017-09-08 22:56:43 +0300

    Created Comments component, enhanced Single component. Created comments reducer.

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
-const Single = () => (
-    <div className='single-photo'>
-        I'm the Single
-    </div>
-);
+const Single = (props) => {
+    const {item_id} = props.params,
+        i = props.posts.findIndex((post) => post.code === item_id);
+
+    const post = props.posts[i],
+        postComments = props.comments[item_id] || [];
+
+    return (
+        <div className='single-photo'>
+            <Photo
+                {...props}
+                i={i}
+                post={post}
+            />
+            <Comments postComments={postComments} {...props} />
+        </div>
+    );
+};
 
 export default Single;
